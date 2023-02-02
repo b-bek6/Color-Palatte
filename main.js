d127e357471f1907ff9e1d6f5582d14cf517b056
Bibek Ghimire <binekghimire555@gmail.com>
2023-02-02 21:13:43 +0545

    refresh-btn

---
+++ main.js
@@ -0,0 +1,15 @@
+const refreshBtn = document.querySelector(".refresh-btn");
+
+const generatePalette = () => {
+    for (let i = 0; i < rray.length; i++){
+        const element = array[i];
+    }
+}
+// generating a ramdom hex color code
+const generatePalette = () => {
+    let randomHex = Math.floor(Math.random()* 0xffffff).toString(16);
+    randomHex = `#${randomHex.padStar(6,"0")}`;
+    console.log(randomHex)
+}
+
+refreshBtn.addEventListener("click", generatePalette);
